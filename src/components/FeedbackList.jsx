25aff1938f31106ecafe75822b53f961b2e10266
Mateusz Melaniuk <mateusz.melaniuk98@gmail.com>
2022-06-18 15:49:04 +0200

    Added JSON Database

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -3,15 +3,18 @@ import { motion, AnimatePresence } from "framer-motion";
 
 import FeedbackItem from "./FeedbackItem";
 import FeedbackContext from "../context/FeedbackContext";
+import Spinner from "./shared/Spinner";
 
 function FeedbackList() {
-  const { feedback } = useContext(FeedbackContext);
+  const { feedback, isLoading } = useContext(FeedbackContext);
 
-  if (!feedback || feedback.lenght === 0) {
+  if (!isLoading && (!feedback || feedback.lenght === 0)) {
     return <p>no feedback yet</p>;
   }
 
-  return (
+  return isLoading ? (
+    <Spinner />
+  ) : (
     <div className="feedback-list">
       <AnimatePresence>
         {feedback.map((item) => (
